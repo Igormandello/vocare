e43b2b6ec78c16d09ce0172e90fa0dc4e25f70d8
Igor Mandello <igormandello@gmail.com>
2018-09-12 23:40:10 -0300

    Implements Login page

---
+++ src/pages/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Header from '../components/Header';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import UserSocialMedias from '../components/UserSocialMedias';
+import Footer from '../components/Footer';
+import '../css/Contact.css';
+
+function Login() {
+  return (
+    <div className="login">
+      <Header />
+      <section>
+        <div>
+          <h1>Login</h1>
+          <Input label="Usuário"/>
+          <Input label="Senha"/>
+          <Input value="Entrar" type="submit" />
+          <p>ou acesse com:</p>
+          <UserSocialMedias />
+        </div>
+        <div>
+          <h1>Não possui uma conta?</h1>
+          <p>Se você não possui uma conta, basta se cadastrar gratuitamente!</p>
+          <Button text="Cadastre-se" fill/>
+        </div>
+      </section>
+      <Footer fill/>
+    </div>
+  );
+}
+
+export default Login;
