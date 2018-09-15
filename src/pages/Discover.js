4b471a6e2264ed65581494808d09bb2fbefdd1c5
Igor Mandello <igormandello@gmail.com>
2018-09-15 16:27:00 -0300

    Implements the Discover page

---
+++ src/pages/Discover.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import UserHeader from '../components/UserHeader';
+import ComboBox from '../components/ComboBox';
+import Footer from '../components/Footer';
+import '../css/Discover.css';
+
+function Discover() {
+  return (
+    <div className="discover">
+      <UserHeader />
+      <section>
+        <h1>Conhecendo profissões</h1>
+        <p>
+          Nessa seção você poderá descobrir áreas que nunca imaginou 
+          que existiam, quem sabe não acaba achando uma que gosta?
+        </p>
+      </section>
+      <section>
+        <ComboBox label="area" options={['a', 'b']}/>
+        <h2>Sua jornada começa aqui</h2>
+        <div>
+          <span></span>
+          <span></span>
+        </div>
+        <img src="computingEngineering.png" alt="computing engineering" />
+        <div>
+          <span></span>
+          <span></span>
+        </div>
+        <img src="mechanicalEngineering.png" alt="mechanical engineering" />
+        <div>
+          <span></span>
+          <span></span>
+        </div>
+        <img src="medicine.png" alt="medicine" />
+      </section>
+      <Footer fill />
+    </div>
+  );
+}
+
+export default Discover;
