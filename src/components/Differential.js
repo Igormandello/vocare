47d471f3e9a6386d831fab519ee36f520ddf4a58
Igormandello <igormandello@gmail.com>
2018-09-17 14:34:14 -0300

    Initializes About page implementation

--- src/components/Differential.js
+++ src/components/Differential.js
@@ -4,11 +4,13 @@ import '../css/Differential.css';
 function Differential(props) {
   return (
     <div className="differential">
-        <img src={props.image} alt={props.title} />
-        <div>
-            <h3>{props.title}</h3>
-            <p>{props.text}</p>
-        </div>
+      <div>
+        <img src={require('./assets/' + props.image)} alt={props.title} />
+      </div>
+      <div>
+        <h3>{props.title}</h3>
+        <p>{props.text}</p>
+      </div>
     </div>
   );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -9,8 +9,8 @@ function About() {
   return (
     <div className="about">
       <Header />
-      <h1>Sobre</h1>
-      <section>
+      <section className="problem">
+        <h1>Sobre</h1>
         <h2>Problema</h2>
         <div>
           <div>
@@ -26,9 +26,6 @@ function About() {
               popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", 
               comes from a line in section 1.10.32.'
             </p>
-          </div>
-          <div>
-            <img src="wrongway.png" alt="wrongway"/>
             <p>
               Contrary to popular belief, Lorem Ipsum is not simply random text. 
               It has roots in a piece of classical Latin literature from 45 BC, making 
@@ -42,9 +39,21 @@ function About() {
               comes from a line in section 1.10.32.'
             </p>
           </div>
+          <div>
+            <img src={require('../imgs/wrongway.jpg')} alt="wrongway"/>
+            <p>
+              Contrary to popular belief, Lorem Ipsum is not simply random text. 
+              It has roots in a piece of classical Latin literature from 45 BC, making 
+              it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney 
+              College in Virginia, looked up one of the more obscure Latin words, consectetur, 
+              from a Lorem Ipsum passage, and going through the cites of the word in classical 
+              literature, discovered the undoubtable source. Lorem Ipsum comes from sections 
+              1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
+            </p>
+          </div>
         </div>
       </section>
-      <section>
+      <section className="differentials">
         <h2>Diferenciais</h2>
         <Differential {...differentials.autoconhecimento}/>
         <Differential {...differentials.ambienteDeDiscussao}/>
@@ -62,21 +71,40 @@ function About() {
 
 const differentials = {
   autoconhecimento: {
-    image: 'reading.png',
+    image: 'reading.jpg',
     title: 'Autoconhecimento',
-    text: 'Lorem'
+    text: `It is a long established fact that a reader will be distracted by the 
+      readable content of a page when looking at its layout. The point of using 
+      Lorem Ipsum is that it has a more-or-less normal distribution of letters, 
+      as opposed to using 'Content here, content here', making it look like readable 
+      English. Many desktop publishing packages and web page editors now use Lorem 
+      Ipsum as their default model text, and a search for 'lorem ipsum' will uncover 
+      many web sites still in their infancy. Various versions have evolved over 
+      the years, sometimes by accident, sometimes on purpose (injected humour and the like).`
   },
 
   ambienteDeDiscussao: {
-    image: 'discussion.png',
+    image: 'discussion.jpg',
     title: 'Ambiente de Discussão',
-    text: 'Lorem'
+    text: `It is a long established fact that a reader will be distracted by the 
+    readable content of a page when looking at its layout. The point of using 
+    Lorem Ipsum is that it has a more-or-less normal distribution of letters, 
+    as opposed to using 'Content here, content here', making it look like readable 
+    English. Many desktop publishing packages and web page editors now use Lorem 
+    Ipsum as their default model text, and a search for 'lorem ipsum'`
   },
 
   conhecimentoDeProfissoes: {
-    image: 'computer.png',
+    image: 'computer.jpg',
     title: 'Conhecimento de Profissões',
-    text: 'Lorem'
+    text: `It is a long established fact that a reader will be distracted by the 
+    readable content of a page when looking at its layout. The point of using 
+    Lorem Ipsum is that it has a more-or-less normal distribution of letters, 
+    as opposed to using 'Content here, content here', making it look like readable 
+    English. Many desktop publishing packages and web page editors now use Lorem 
+    Ipsum as their default model text, and a search for 'lorem ipsum' will uncover 
+    many web sites still in their infancy. Various versions have evolved over 
+    the years, sometimes by accident, sometimes on purpose (injected humour and the like).`
   }
 }
 
