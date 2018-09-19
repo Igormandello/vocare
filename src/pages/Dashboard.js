750b1837291f0d51fc33139a7631d7151ba4af5c
Igor Mandello <igormandello@gmail.com>
2018-09-18 23:31:08 -0300

    Implements the Dashboard style
    - The two extra card in the right section aren't implemented yet

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -8,53 +8,64 @@ function Dashboard() {
   return (
     <div className="dashboard">
       <UserHeader />
-      <section>
+      <section className="userData">
         <h1>Bem vindo, Igor</h1>
         <div>
           <span>Seu nível atual: 3</span>
-          <div>
+          <div className="level">
             <span></span>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div className="active"></div>
+            <div className="active"></div>
+            <div className="active"></div>
             <div></div>
             <div></div>
           </div>
         </div>
       </section>
-      <section>
+      <section class="objectives">
         <div>
-          <h2>Objetivo atual</h2>
-          <p>
-            Você está em dúvida entre algumas profissões, seu objetivo agora
-            é decidir entre essas qual você quer que seja sua área profissional, 
-            essa é uma decisão bem importante e difícil, então tome seu tempo e 
-            não se apresse, para que faça a melhor decisão possível.
-          </p>
-          <p>
-            Você pode saber mais sobre cada área que está em dúvida acessando as 
-            páginas respectivas a elas:
-          </p>
-          <Button text="Ciência da Computação" />
-          <Button text="Engenharia da Computação" />
-          <Button text="Análise e Desenvolvimento" />
+          <div>
+            <h2>Objetivo atual</h2>
+            <p>
+              Você está em dúvida entre algumas profissões, seu objetivo agora
+              é decidir entre essas qual você quer que seja sua área profissional, 
+              essa é uma decisão bem importante e difícil, então tome seu tempo e 
+              não se apresse, para que faça a melhor decisão possível.
+            </p>
+            <p>
+              Você pode saber mais sobre cada área que está em dúvida acessando as 
+              páginas respectivas a elas:
+            </p>
+          </div>
 
-          <p>
-            Também é possível debater com os outros usuários do site para decidir 
-            qual a profissão que mais combina com você em nosso espaço de discussão.
-          </p>
-          <Button text="Acessar Fórum" />
+          <div className="courses">
+            <Button text="Engenharia da Computação" />
+            <Button text="Ciência da Computação" />
+            <Button text="Análise e Desenvolvimento de Sistemas" />
+          </div>
+
+          <div>
+            <p>
+              Também é possível debater com os outros usuários do site para decidir 
+              qual a profissão que mais combina com você em nosso espaço de discussão.
+            </p>
+            <Button text="Acessar Fórum" />
+          </div>
         </div>
         <div>
           <h2>O que estão debatendo...</h2>
-          <div>
+          <div className="post card">
             <div>
-              <img src="bart.png" alt="user" />
-              <h3>O que é melhor? Ciência da Computação ou Engengaria?</h3>
-              <p>
-                Lorem ipsum dolor site amet, consectetur adipiscing elit, sed 
-                do elusmod tempor incididunt ut labore et dolore magna...
-              </p>
+              <div>
+                <img src={require('../components/assets/bart.jpg')} alt="user" className="card"/>
+              </div>
+              <div>
+                <h3>O que é melhor? Ciência da Computação ou Engenharia?</h3>
+                <p>
+                  Lorem ipsum dolor site amet, consectetur adipiscing elit, sed 
+                  do elusmod tempor incididunt ut labore et dolore magna...
+                </p>
+              </div>
             </div>
             <Button text="Acesse essa discussão" fill/>
           </div>
