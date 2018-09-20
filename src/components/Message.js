738c2d7d82eca75ed273d9e67a89d91037e94a4b
Igor Mandello <igormandello@gmail.com>
2018-09-20 00:58:53 -0300

    Creates the style for Message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,15 +4,15 @@ import '../css/Message.css';
 function Message(props) {
   return (
     <div className="message">
-      <div>
-        <div>
-          <img src={props.user.image} alt="user"/>
+      <div className="userProfile">
+        <div className="avatar">
+          <img src={require('./assets/' + props.user.image)} alt="user"/>
           <span>{props.user.level}</span>
         </div>
-        <p>{props.user.name}</p>
+        <p className="username">{props.user.name}</p>
         <p>{props.user.messages} mensagens</p>
       </div>
-      <div>
+      <div className="messageData">
         {props.title && <h1>{props.title}</h1>}
         <label>{props.date} - {props.time}</label>
         <p>{props.message}</p>
