5224f7b9bb94c4b89e75274982f3b01d760d58af
Igormandello <igormandello@gmail.com>
2018-09-20 08:00:16 -0300

    Adds links to the social medias

--- src/components/SocialMedias.js
+++ src/components/SocialMedias.js
@@ -5,10 +5,10 @@ import '../css/SocialMedias.css';
 function SocialMedias(props) {
   return (
     <ul className={'socialMedias' + (props.invert ? ' inverse' : '')}>
-      <Button href="https://twitter.com" newTab>
+      <Button href="https://twitter.com/vocare8" newTab>
         <img src={require('./assets/twitter.svg')} alt="twitter"/>
       </Button>
-      <Button href="https://facebook.com" newTab>
+      <Button href="https://facebook.com/vocarectc" newTab>
         <img src={require('./assets/facebook.svg')} alt="facebook"/>
       </Button>
     </ul>
