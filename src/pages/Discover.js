868b750ee23642041c2acab6f8fe926cd91fbcb8
Igormandello <igormandello@gmail.com>
2018-09-24 11:36:21 -0300

    Adds a popover package to Discover screen
    - The react-popover package will be used only in the discover screen becase it's methods fits better there

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,52 +1,95 @@
 import React from 'react';
 import UserHeader from '../components/UserHeader';
 import ComboBox from '../components/ComboBox';
+import Popover from 'react-popover';
 import Footer from '../components/Footer';
 import '../css/Discover.css';
 
-function Discover() {
-  return (
-    <div className="discover">
-      <UserHeader />
-      <section className="info">
-        <h1>Conhecendo profissões</h1>
-        <p>
-          Nessa seção você poderá descobrir áreas que nunca imaginou 
-          que existiam, quem sabe não acaba achando uma que gosta?
-        </p>
-      </section>
-      <section className="professions">
-        <ComboBox label="Área:" options={['a', 'b']}/>
-        <h2>Sua jornada começa aqui</h2>
-        <div className="path">
-          <div className="profession">
-            <div>
-              <span></span>
-              <span></span>
+class Discover extends React.Component {
+  state = {};
+
+  popoverTrigger = (event) => {
+    let id = event.currentTarget.id;
+
+    this.setState((prev) => {
+      let ret = { ...prev };
+      ret[id] = !prev[id];
+
+      return ret;
+    })
+  }
+
+  render() {
+    return (
+      <div className="discover">
+        <UserHeader />
+        <section className="info">
+          <h1>Conhecendo profissões</h1>
+          <p>
+            Nessa seção você poderá descobrir áreas que nunca imaginou 
+            que existiam, quem sabe não acaba achando uma que gosta?
+          </p>
+        </section>
+        <section className="professions">
+          <ComboBox label="Área:" options={['a', 'b']}/>
+          <h2>Sua jornada começa aqui</h2>
+          <div className="path">
+            <div className="profession">
+              <div>
+                <span></span>
+                <span></span>
+              </div>
+              <Popover isOpen={this.state.computerEngineering} place="right" body={popoversData.computerEngineering}>
+                <a id="computerEngineering" onClick={this.popoverTrigger}>
+                  <img src={require('../imgs/computerEngineering.jpg')} alt="computer engineering" />
+                </a>
+              </Popover>
             </div>
-            <a href="/vocare/aboutCourse">
-              <img src={require('../imgs/computerEngineering.jpg')} alt="computer engineering" />
-            </a>
-          </div>
-          <div className="profession">
-            <div>
-              <span></span>
-              <span></span>
+            <div className="profession">
+              <div>
+                <span></span>
+                <span></span>
+              </div>
+              <Popover isOpen={this.state.mechanicalEngineering} place="left" body={popoversData.mechanicalEngineering}>
+                <a id="mechanicalEngineering" onClick={this.popoverTrigger}>
+                  <img src={require('../imgs/mechanicalEngineering.jpg')} alt="mechanical engineering" />
+                </a>
+              </Popover>
             </div>
-            <img src={require('../imgs/mechanicalEngineering.jpg')} alt="mechanical engineering" />
-          </div>
-          <div className="profession">
-            <div>
-              <span></span>
-              <span></span>
+            <div className="profession">
+              <div>
+                <span></span>
+                <span></span>
+              </div>
+              <Popover isOpen={this.state.medicine} place="right" body={popoversData.medicine}>
+                <a id="medicine" onClick={this.popoverTrigger}>
+                  <img src={require('../imgs/medicine.jpg')} alt="medicine" />
+                </a>
+              </Popover>
             </div>
-            <img src={require('../imgs/medicine.jpg')} alt="medicine" />
           </div>
-        </div>
-      </section>
-      <Footer fill />
-    </div>
-  );
+        </section>
+        <Footer fill />
+      </div>
+    );
+  }
+}
+
+const popoversData = {
+  computerEngineering: [
+    <h3>Engenharia da Computação</h3>,
+    <p>Lorem ipsum dolor sit amet</p>
+  ],
+
+  mechanicalEngineering: [
+    <h3>Engenharia Mecânica</h3>,
+    <p>Lorem ipsum dolor sit amet</p>
+  ],
+
+  medicine: [
+    <h3>Medicina</h3>,
+    <p>Lorem ipsum dolor sit amet</p>
+  ]
 }
 
 export default Discover;
