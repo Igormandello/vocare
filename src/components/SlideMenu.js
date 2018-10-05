0c09e8713c08c273e2eec1289596b576afb5c6d7
Igor Mandello <igormandello@gmail.com>
2018-10-04 23:45:15 -0300

    Finishes the SlideMenu styling and scripting

--- src/components/SlideMenu.js
+++ src/components/SlideMenu.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../css/SlideMenu.css';
 
-function SlideMenu(props) {
-  return (
-    <div className="slideMenu">
-      <div className="menu-overlay"/>
-      <div className="menu">
-        {props.children}
+class SlideMenu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false
+    };
+  }
+
+  openMenu = () => {
+    this.setState(() => {
+      return {
+        open: true
+      };
+    });
+  }
+
+  closeMenu = () => {
+    this.setState(() => {
+      return {
+        open: false
+      };
+    });
+  }
+
+  render() {
+    return (
+      <div className={'slideMenu' + (this.state.open ? ' open' : '')}>
+        <div className="menu-overlay" onClick={this.closeMenu}/>
+        <div className="menu">
+          {this.props.children}
+        </div>
+        <div className="menu-button" onClick={this.openMenu}>
+          <span/>
+          <span/>
+          <span/>
+        </div>
       </div>
-      <div className="menu-button">
-        <span/>
-        <span/>
-        <span/>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default SlideMenu;
