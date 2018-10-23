1c3c86e951545fd2c80ca5d4f7446c0779d2baf3
Vitor Bartier <vitorbartier@hotmail.com>
2018-10-23 10:02:20 -0300

    Adds gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+sass.compiler = require('node-sass');
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/sass/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'sass:watch']);
