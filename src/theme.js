cf9fbea9cd1eebd648a6913fbdd94ecbc4a79b1b
Igormandello <igormandello@gmail.com>
2018-11-10 11:06:28 -0200

    Creates a theme file
    - This file specifies the material color pallete used

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import deepPurple from '@material-ui/core/colors/deepPurple';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: deepPurple.A200
+    },
+    secondary: {
+      main: deepPurple.A200
+    }
+  },
+});
+
+export default theme;
