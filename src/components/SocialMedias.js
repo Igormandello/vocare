09f685628a300ccbe9dc733ec6f32e2ad5265b94
Igormandello <igormandello@gmail.com>
2018-11-14 08:34:24 -0200

    Changes the footer buttons for icon buttons

--- src/components/SocialMedias.js
+++ src/components/SocialMedias.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Icon from '@material-ui/core/Icon';
 import Button from './Button';
 import '../css/SocialMedias.css';
 
 function SocialMedias(props) {
   return (
     <ul className={'socialMedias' + (props.invert ? ' inverse' : '')}>
-      <Button href="https://twitter.com/vocare8" newTab>
+      <IconButton color={props.invert ? 'secondary' : 'primary'} target="_blank" href="https://twitter.com/vocare8" aria-label="Access our Twitter">
         <img src={require('./assets/twitter.svg')} alt="twitter"/>
-      </Button>
-      <Button href="https://facebook.com/vocarectc" newTab>
+      </IconButton>
+      <IconButton color={props.invert ? 'secondary' : 'primary'} target="_blank" href="https://facebook.com/vocarectc" aria-label="Access our Facebook">
         <img src={require('./assets/facebook.svg')} alt="facebook"/>
-      </Button>
+      </IconButton>
     </ul>
   );
 }
