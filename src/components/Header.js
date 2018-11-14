561509eaad4c5f125053691b8020dfe2c4991562
Igormandello <igormandello@gmail.com>
2018-11-14 08:39:27 -0200

    Adds elevation in header and fixes buttons colors in login and signup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import Paper from '@material-ui/core/Paper';
 import SlideMenu from './SlideMenu';
 import Button from './Button';
 import '../css/Header.css';
 
 function Header() {
   return (
-    <header className="header">
-      <SlideMenu>
-      </SlideMenu>
-      <nav>
-        <Button variant="text" color="secondary" href="/vocare/about" text="Sobre o projeto"/>
-        <Button variant="text" color="secondary" href="/vocare/contact" text="Contato"/>
-      </nav>
-      <a href="/vocare/">
-        VOcAre
-      </a>
-      <nav>
-        <Button variant="text" color="secondary" href="/vocare/login" text="Login"/>
-        <Button color="secondary" href="/vocare/signup" text="Cadastre-se" noShadow/>
-      </nav>
-    </header>
+    <Paper elevation={4}>
+      <header className="header">
+        <SlideMenu>
+        </SlideMenu>
+        <nav>
+          <Button variant="text" color="secondary" href="/vocare/about" text="Sobre o projeto"/>
+          <Button variant="text" color="secondary" href="/vocare/contact" text="Contato"/>
+        </nav>
+        <a href="/vocare/">
+          VOcAre
+        </a>
+        <nav>
+          <Button variant="text" color="secondary" href="/vocare/login" text="Login"/>
+          <Button color="secondary" href="/vocare/signup" text="Cadastre-se" noShadow/>
+        </nav>
+      </header>
+    </Paper>
   );
 }
 
