7676efa44935e9eb024bc4c31be67fb8f4f598dc
Igor Mandello <igormandello@gmail.com>
2018-11-15 19:33:42 -0200

    Finishes the settings page style

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Icon from '@material-ui/core/Icon';
 import UserHeader from '../components/UserHeader';
 import UserSocialMedias from '../components/UserSocialMedias';
 import Input from '../components/Input';
@@ -12,11 +15,13 @@ function Settings() {
       <UserHeader />
       <section>
         <h1>Configurações</h1>
-        <div className="avatar card">
+        <div className="avatar">
           <img src={require('../components/assets/igor.jpg')} alt="profile"/>
-          <Button>
-            <img src={require('../components/assets/edit.svg')} alt="edit" />
-          </Button>
+          <div>
+            <IconButton color="primary">
+              <Icon>edit</Icon>
+            </IconButton>
+          </div>
         </div>
         <p className="divider">conectado com:</p>
         <UserSocialMedias actives={[true, true]}/>
