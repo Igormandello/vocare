ea94e3084b5e2980a96e93ce5908949a29e1009f
Igormandello <igormandello@gmail.com>
2018-11-21 16:31:12 -0200

    Creates the store with middleware

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import reducer from '../reducers/index';
+import mySaga from '../sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(mySaga);
+export default store;
