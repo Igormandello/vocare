22ed352509a48fbba9be9f22e3f080ea5f7798f0
Igor Mandello <igormandello@gmail.com>
2018-11-20 23:13:33 -0200

    Adds a List to the SlideMenu in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Icon from '@material-ui/core/Icon';
+import { Link } from 'react-router-dom';
 import SlideMenu from './SlideMenu';
 import Button from './Button';
 import '../css/Header.css';
@@ -9,6 +16,41 @@ function Header() {
     <Paper elevation={4}>
       <header className="header">
         <SlideMenu>
+          <List>
+            <Link to="/vocare/about">
+              <ListItem button>
+                  <ListItemIcon>
+                    <Icon>info</Icon>
+                  </ListItemIcon>
+                  <ListItemText primary="Sobre o projeto"/>
+              </ListItem>
+            </Link>
+            <Link to="/vocare/contact">
+              <ListItem button>
+                <ListItemIcon>
+                  <Icon>email</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Contato"/>
+              </ListItem>
+            </Link>
+            <Divider />
+            <Link to="/vocare/login">
+              <ListItem button>
+                <ListItemIcon>
+                  <Icon>account_circle</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Login"/>
+              </ListItem>
+            </Link>
+            <Link to="/vocare/signup">
+              <ListItem button>
+                <ListItemIcon>
+                  <Icon>person_add</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Cadastro"/>
+              </ListItem>
+            </Link>
+          </List>
         </SlideMenu>
         <nav>
           <Button variant="text" color="secondary" href="/vocare/about" text="Sobre o projeto"/>
