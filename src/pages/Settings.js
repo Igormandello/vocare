1c780948ea9c15767d6af340ee3cfe45476bccdb
Igor Mandello <igormandello@gmail.com>
2018-12-03 01:01:25 -0200

    Starts the image editing implementation

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Icon from '@material-ui/core/Icon';
 import Avatar from '@material-ui/core/Avatar';
@@ -18,47 +18,64 @@ const styles = theme => ({
   bigAvatar: {
     width: 'inherit',
     height: 'inherit'
+  },
+  hiddenInput: {
+    display: 'none'
   }
 });
 
-function Settings(props) {
-  const { classes, user } = props;
-  const { username, profile_picture, email } = user;
+class Settings extends Component {
+  componentDidMount() {
+    document.querySelector('#uploadImage').addEventListener('change', function() {
+      if (this.files && this.files[0]) {
+        let url = window.URL.createObjectURL(this.files[0]);
+        console.log(url);
+      }
+    });
+  }
 
-  let userPic;
-  if (profile_picture)
-    userPic = <Avatar src={props.user.profile_picture} className={classes.bigAvatar}/>;
-  else
-    userPic = <Avatar className={[classes.avatar, classes.bigAvatar]}>{username.split(' ')[0][0]}</Avatar>;
+  render() {
+    const { classes, user } = this.props;
+    const { username, profile_picture, email } = user;
 
-  return (
-    <div className="settings">
-      <UserHeader />
-      <section>
-        <h1>Configurações</h1>
-        <div className="avatar">
-          {userPic}
-          <div className="edit">
-            <Button color="primary">
-              <Icon>edit</Icon>
-            </Button>
+    let userPic;
+    if (profile_picture)
+      userPic = <Avatar src={profile_picture} className={classes.bigAvatar}/>;
+    else
+      userPic = <Avatar className={[classes.avatar, classes.bigAvatar]}>{username.split(' ')[0][0]}</Avatar>;
+
+    return (
+      <div className="settings">
+        <UserHeader />
+        <section>
+          <h1>Configurações</h1>
+          <div className="avatar">
+            {userPic}
+            <div className="edit">
+              <Button onClick={() => document.querySelector('#uploadImage').click()} color="primary">
+                <Icon>edit</Icon>
+              </Button>
+            </div>
           </div>
-        </div>
-        <p className="divider">conectado com:</p>
-        <UserSocialMedias actives={[true, true]}/>
-        <Input label="Nome"/>
-        <Input label="E-mail" type="email"/>
-        <Input label="Senha" type="password"/>
-        <Input label="Confirmar senha" type="password"/>
+          <p className="divider">conectado com:</p>
+          <UserSocialMedias actives={[true, true]}/>
+          <div className={classes.hiddenInput}>
+            <Input id="uploadImage" type="file"/>
+          </div>
+          <Input label="Nome" defaultValue={username}/>
+          <Input label="E-mail" type="email" defaultValue={email}/>
+          <Input label="Senha" type="password"/>
+          <Input label="Confirmar senha" type="password"/>
 
-        <div className="options">
-          <Button text="Salvar" href="/vocare/dashboard" />
-          <Button text="Excluir conta"/>
-        </div>
-      </section>
-      <Footer fill />
-    </div>
-  );
+          <div className="options">
+            <Button text="Salvar" href="/vocare/dashboard" />
+            <Button text="Excluir conta"/>
+          </div>
+        </section>
+        <Footer fill />
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(connect(
