c999c6b393e85d06b2d29d2492111ebd1ef449c1
Igor Mandello <igormandello@gmail.com>
2018-12-05 00:49:07 -0200

    Adds action to load a single post and its comments

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -6,6 +6,10 @@ export const LOAD_MORE_REQUESTED = 'LOAD_MORE_POSTS_REQUESTED';
 export const LOAD_MORE_SUCCEEDED = 'LOAD_MORE_POSTS_SUCCEEDED';
 export const LOAD_MORE_FAILED = 'LOAD_MORE_POSTS_FAILED';
 
+export const VIEW_POST_REQUESTED = 'VIEW_POST_REQUESTED';
+export const VIEW_POST_SUCCEEDED = 'VIEW_POST_SUCCEEDED';
+export const VIEW_POST_FAILED = 'VIEW_POST_FAILED';
+
 export const fetchPosts = () => ({
   type: POSTS_REQUESTED
 });
@@ -14,3 +18,9 @@ export const loadMorePosts = (pageOffset) => ({
   type: LOAD_MORE_REQUESTED,
   pageOffset
 });
+
+export const viewPost = (id, access_token) => ({
+  type: VIEW_POST_REQUESTED,
+  id,
+  access_token
+})
