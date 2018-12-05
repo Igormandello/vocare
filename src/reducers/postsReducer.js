bba560695a0ab785761d2b8a9149a5dc34745067
Igor Mandello <igormandello@gmail.com>
2018-12-05 00:49:43 -0200

    Adds the current post to the posts state

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,10 +1,12 @@
 import {
   POSTS_SUCCEEDED,
   POSTS_FAILED,
-  LOAD_MORE_SUCCEEDED
+  LOAD_MORE_SUCCEEDED,
+  VIEW_POST_SUCCEEDED,
+  VIEW_POST_FAILED
 } from '../actions/postsActions';
 
-function posts(state = { posts: [], loaded: false, error: false, nextPage: 1 }, action) {
+function posts(state = { posts: [], loadedPost: { comments: [] }, loaded: false, error: false, nextPage: 1 }, action) {
   switch (action.type) {
     case POSTS_SUCCEEDED:
       return {
@@ -19,7 +21,17 @@ function posts(state = { posts: [], loaded: false, error: false, nextPage: 1 }, 
         posts: state.posts.concat(action.posts),
         nextPage: state.nextPage + 1
       };
+    case VIEW_POST_SUCCEEDED:
+      return {
+        ...state,
+        loaded: true,
+        loadedPost: {
+          ...action.post,
+          comments: action.comments
+        }
+      };
     case POSTS_FAILED:
+    case VIEW_POST_FAILED:
       return {
         ...state,
         error: true
